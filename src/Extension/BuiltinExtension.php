96934a319ee489035a1675301f7c654d1524657c
Bart Jaskulski <bjaskulski@protonmail.com>
2024-03-10 01:46:20 +0100

    refactor: simplify definitions loader instantation
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Extension/BuiltinExtension.php
+++ src/Extension/BuiltinExtension.php
@@ -5,22 +5,16 @@ declare(strict_types=1);
 namespace WPDesk\Init\Extension;
 
 use Psr\Container\ContainerInterface;
-use WPDesk\Init\Binding\DefinitionFactory;
 use WPDesk\Init\Binding\Loader\BindingDefinitions;
 use WPDesk\Init\Binding\Loader\FilesystemDefinitions;
 use WPDesk\Init\Configuration\ReadableConfig;
 use WPDesk\Init\DependencyInjection\ContainerBuilder;
-use WPDesk\Init\Loader\PhpFileLoader;
 use WPDesk\Init\Plugin\Plugin;
 
 class BuiltinExtension implements Extension {
 
-	public function bindings(ContainerInterface $c): BindingDefinitions {
-		return new FilesystemDefinitions(
-			__DIR__ . '/../Resources/bindings',
-			new PhpFileLoader(),
-			new DefinitionFactory()
-	   	);
+	public function bindings( ContainerInterface $c ): BindingDefinitions {
+		return new FilesystemDefinitions( __DIR__ . '/../Resources/bindings' );
 	}
 
 	public function build( ContainerBuilder $builder, Plugin $plugin, ReadableConfig $config ): void {
