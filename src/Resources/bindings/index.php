41ca90a6db0a8139df17dadb69455079dc21ae29
Bart Jaskulski <bjaskulski@protonmail.com>
2024-08-13 08:59:12 +0200

    fix: typo in binding class name
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Resources/bindings/index.php
+++ src/Resources/bindings/index.php
@@ -1,9 +1,9 @@
 <?php
 
+use WPDesk\Init\Extension\CommonBinding\CustomOrdersTableCompatibility;
 use WPDesk\Init\Extension\CommonBinding\I18n;
-use WPDesk\Init\Extension\CommonBinding\CustomOrderTableCompatibility;
 
 return [
 	I18n::class,
-	CustomOrderTableCompatibility::class,
+	CustomOrdersTableCompatibility::class,
 ];
