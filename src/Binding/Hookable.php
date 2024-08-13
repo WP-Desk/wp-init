83fc4e6d46ead018a31e3167948da2a6adc7bd79
Bart Jaskulski <bjaskulski@protonmail.com>
2024-08-13 08:49:15 +0200

    fix: improve compatibility with/without wp-builder
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Binding/Hookable.php
+++ src/Binding/Hookable.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace WPDesk\Init\Binding;
 
-interface Hookable extends \WPDesk\PluginBuilder\Plugin\Hookable {
-
-	public function hooks(): void;
+/**
+ * Operate in mode compatible with wp-builder library, as wp-init roots on it and needs to preserve the easy way to upgrade existing clients.
+ */
+if ( class_exists( \WPDesk\PluginBuilder\Plugin\Hookable::class ) ) {
+	interface Hookable extends \WPDesk\PluginBuilder\Plugin\Hookable {
+		public function hooks(): void;
+	}
+} else {
+	interface Hookable {
+		public function hooks(): void;
+	}
 }
