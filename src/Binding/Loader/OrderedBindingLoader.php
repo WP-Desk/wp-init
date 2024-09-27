9533440fb99586cfafc92c6b01bbc09549d14a53
Bart Jaskulski <bjaskulski@protonmail.com>
2024-09-27 12:07:14 +0200

    feat: add ordered loader with priority option
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

---
+++ src/Binding/Loader/OrderedBindingLoader.php
@@ -0,0 +1,27 @@
+<?php
+declare( strict_types=1 );
+
+namespace WPDesk\Init\Binding\Loader;
+
+final class OrderedBindingLoader implements BindingDefinitions {
+
+	private BindingDefinitions $loader;
+
+	public function __construct( BindingDefinitions $loader ) {
+		$this->loader = $loader;
+	}
+
+	public function load(): iterable {
+		$definitions = [];
+		foreach ( $this->loader->load() as $def ) {
+			$definitions[] = $def;
+		}
+
+		usort(
+			$definitions,
+			fn ( $a, $b ) => $a->option( 'priority' ) <=> $b->option( 'priority' )
+		);
+
+		yield from array_reverse( $definitions, false );
+	}
+}
